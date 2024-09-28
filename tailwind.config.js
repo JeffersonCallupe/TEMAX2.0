bc665ffcfb175eff603bff3c5fa96e97b54d5820
Jefferson <jeff.callupe@gmail.com>
2024-09-28 09:49:23 -0500

    segunda version

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'celeste-claro': '#129EB9',
+        'azul-verduzco': '#1B4B5E',
+        'negro-footer': '#232425',
+        'gradiente-azul': '#2C5075',
+        'gradiente-negro': '#232425',
+        'gris-letras':'#555555'
+      }
+
+
+    },
+  },
+  plugins: [],
+}
+
